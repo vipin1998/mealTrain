f09f84dcef49a7d1adfac2274ed77f2a345e0cc2
vipin1998 <vipindhangar1998@gmail.com>
2018-02-01 11:03:06 +0530

    leaders ship services are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
-
+import {LeaderService} from './services/leader.service'
 import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
@@ -49,7 +49,7 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
     AppRoutingModule
 
   ],
-  providers: [ DishService,PromotionService ],
+  providers: [ DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/leader.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Leader } from '../shared/leader';
+import { LEADERS } from '../shared/leaders';
+
+@Injectable()
+export class LeaderService {
+
+  constructor() { }
+
+  getLeaders() : Leader[]
+  {
+    return LEADERS;
+  }
+ 
+  getFeaturedLeader() : Leader{
+    return LEADERS.filter((ld) =>(ld.featured))[0];
+  }
+
+}
